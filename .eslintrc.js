bffef0f624e4a005978886e35e657c2ae3dee06d
jhaemin <io@jhaemin.com>
2020-07-13 16:22:27 +0900

    config(eslint): typescript and other rules

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-extra-semi': [0],
+    '@typescript-eslint/explicit-function-return-type': [0],
+    '@typescript-eslint/no-empty-function': [0],
+    '@typescript-eslint/no-var-requires': [0],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': [0],
+    '@typescript-eslint/camelcase': [0],
+    '@typescript-eslint/explicit-module-boundary-types': [0],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+  },
+}
