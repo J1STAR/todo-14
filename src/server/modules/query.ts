8d9780e8a0bc1faa9c7d334e97aa8f98a134d2a8
dnacu <winteri1276@gmail.com>
2020-07-15 17:05:31 +0900

    chore:query함수에 generics타입 추가

--- src/server/modules/query.ts
+++ src/server/modules/query.ts
@@ -1,6 +1,6 @@
 import { connection } from './connection'
 
-const query = (query: string) => {
+const query = <T>(query: string): Promise<T> => {
   return new Promise((resolve, reject) => {
     connection.query(query, (err, res) => {
       if (err) {
