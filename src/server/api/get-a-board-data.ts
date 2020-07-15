69533587c1c4f17c69d5ed1195f983fcefbc7e34
jhaemin <io@jhaemin.com>
2020-07-16 00:33:42 +0900

    feat(api): get a board data (#21)

---
+++ src/server/api/get-a-board-data.ts
@@ -0,0 +1,65 @@
+import express from 'express'
+import { escape } from '../modules/escape'
+import { query } from '../modules/query'
+
+const router = express.Router()
+
+type GetBoardDataResponseData = {
+  board: {
+    id: number
+    name: string
+    columns: {
+      id: number
+      name: string
+      previousColumnId: number
+      createdAt: string
+      cards: {
+        id: number
+        content: string
+        icon: string
+        previousCardId: number
+        createdAt: string
+        editedAt: string
+      }[]
+    }[]
+  }
+}
+
+router.get('/board/:boardId', async ({ params }, res) => {
+  const boardId = parseInt(params.boardId)
+
+  if (!boardId) {
+    res.sendStatus(400)
+    return
+  }
+
+  // TODO: !!! Use correct types
+
+  const [board] = await query(`SELECT * FROM board WHERE id=${escape(boardId)}`)
+  const columns = (await query(
+    `SELECT id, name, previousColumnId, createdAt from \`column\` WHERE boardId=${escape(
+      boardId
+    )}`
+  )) as []
+
+  for (const column of columns) {
+    const cards = await query(
+      `SELECT id, content, icon, previousCardId, createdAt, editedAt from card WHERE columnId=${
+        (column as any).id
+      }`
+    )
+
+    ;(column as any)['cards'] = cards
+  }
+
+  const responseData: GetBoardDataResponseData = {
+    board: {
+      ...board,
+      columns,
+    },
+  }
+
+  res.json(responseData)
+})
+
+export { router as getABoardDataRouter }
--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { createAColumnRouter } from './create-a-column'
+import { getABoardDataRouter } from './get-a-board-data'
 import { modifyAColumnRouter } from './modify-a-column'
 import { removeAColumnRouter } from './remove-a-column'
 
@@ -8,5 +9,7 @@ const router = express.Router()
 router.use(createAColumnRouter)
 router.use(modifyAColumnRouter)
 router.use(removeAColumnRouter)
+
+router.use(getABoardDataRouter)
 
 export { router as api }
