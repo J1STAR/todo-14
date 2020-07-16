152ad22c7de0293e5b81ae2af6df6632c7156880
dnacu <winteri1276@gmail.com>
2020-07-16 10:02:21 +0900

    feat:get boards list api 작성

---
+++ src/server/api/get-boards-list.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+
+import { query } from '@/server/modules/query'
+
+import { Board as BoardResponse } from '@/types/response'
+
+const router = express.Router()
+
+export type BoardResopnseData = BoardResponse & { authority: string }
+
+export type GetBoardsListResponseData = {
+  boards: BoardResopnseData[]
+}
+
+router.get('/boards', async (req, res) => {
+  // TODO: get from session
+  const userId = 1
+
+  // 가져올 컬럼을 여기서 셋팅하면 generic type은 어떻게...?
+  const boards = await query<BoardResopnseData[]>(
+    `SELECT b.id, b.name, b.createdAt, o.authority from user u
+      JOIN ownership o ON u.id=o.userId
+      JOIN board b ON o.boardId=b.id
+      WHERE u.id=${userId}`
+  )
+
+  if (!boards.length) {
+    res.sendStatus(404)
+    return
+  }
+
+  const result: GetBoardsListResponseData = { boards }
+  res.json(result)
+})
+
+export { router as getBoardsListRouter }
--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
+import { getBoardsListRouter } from './get-boards-list'
 import { createACardRouter } from './create-a-card'
 
 const router = express.Router()
 
+router.use(getBoardsListRouter)
 router.use(createACardRouter)
 
 export { router as api }
