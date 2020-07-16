50c4e4eaefeda4bdb05c9e1990474203f5f8a6b3
dnacu <winteri1276@gmail.com>
2020-07-16 10:02:53 +0900

    feat:modify a card api 작성

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -2,10 +2,12 @@ import express from 'express'
 
 import { getBoardsListRouter } from './get-boards-list'
 import { createACardRouter } from './create-a-card'
+import { modifyACardRouter } from './modify-a-card'
 
 const router = express.Router()
 
 router.use(getBoardsListRouter)
 router.use(createACardRouter)
+router.use(modifyACardRouter)
 
 export { router as api }
