b8599f8dd4b542e14991590776ae3341d227f930
dnacu <winteri1276@gmail.com>
2020-07-18 22:51:23 +0900

    fix:카드의 본문이 없는 경우 대처
    - 본문이 없는 경우를 핸들링 하지 않아 제목, 본문이 복사되는 문제를 해결함
    - 본문이 없을 경우 빈 문자열을 반환하도록 하였다.

--- src/client/utils/content-parser.ts
+++ src/client/utils/content-parser.ts
@@ -1,6 +1,10 @@
 export const parseContent = (content: string): [string, string] => {
   const firstNewLineIdx = content.indexOf('\n')
 
+  if (firstNewLineIdx < 0) {
+    return [content, '']
+  }
+
   return [
     content.slice(0, firstNewLineIdx),
     content.slice(firstNewLineIdx + 1, content.length),
